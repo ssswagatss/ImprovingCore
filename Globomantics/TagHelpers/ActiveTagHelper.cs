623e8b822341a198279605621ea705be5e488c46
Swagat Swain <swagat.swain@mindfiresolutions.com>
2018-09-14 13:37:36 +0530

    Tag helpers andview components

---
+++ Globomantics/TagHelpers/ActiveTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Globomantics.Core.Models;
+using Globomantics.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Globomantics.TagHelpers
+{
+    [HtmlTargetElement("a",Attributes ="active-url")]
+    public class ActiveTafHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _httpContext;
+        public string ActiveUrl { get; set; }
+
+        public ActiveTafHelper(IHttpContextAccessor httpContext)
+        {
+            _httpContext = httpContext;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (_httpContext.HttpContext.Request.Path.ToString().ToLower().Contains(ActiveUrl.ToLower()))
+            {
+                var existingAttributes = output.Attributes["class"]?.Value;
+
+                if (existingAttributes != null)
+                    output.Attributes.SetAttribute("class", "active " + existingAttributes.ToString());
+                else
+                    output.Attributes.SetAttribute("class", "active");
+            }
+        }
+    }
+}
---
+++ Globomantics/TagHelpers/CDRateTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Globomantics.Core.Models;
+using Globomantics.Services;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Globomantics.TagHelpers
+{
+    [HtmlTargetElement("cdrate")]
+    public class CDRateTagHelper : TagHelper
+    {
+        private readonly IRateService _rateService;
+
+
+        public string Title { get; set; }
+        public string MeterPercentage { get; set; }
+        public CDTermLength TermLength { get; set; }
+
+
+        public CDRateTagHelper(IRateService rateService)
+        {
+            _rateService = rateService;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var rate = _rateService.GetCDRateByTerm(TermLength);
+            output.Content.SetHtmlContent(
+                $@"<div class='meter'>
+                    <p>{Title}</p>
+                    <div class='progress'>
+                       <div class='progress-bar bg-info' style='width: {MeterPercentage}%'>{rate}%</div>
+                    </div>
+                </div>"
+                );
+        }
+    }
+}
